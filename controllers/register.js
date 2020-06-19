0fcaa5cfffb79cdedfce8afc0a0f35ab2215cbcd
Alejandro <arico@uc.cl>
2020-06-19 11:54:13 -0400

    Deploying server

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,7 @@
-
-
 const handleRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password) {
-        return res.status(400).json('Incorrect form submition')
+        return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
@@ -22,13 +20,13 @@ const handleRegister = (req, res, db, bcrypt) => {
                         joined: new Date()
                     })
                     .then(user => {
-                        res.json(user[0])
+                        res.json(user[0]);
                     })
-                    .catch(err => res.status(400).json('Unable to register.'))
             })
             .then(trx.commit)
             .catch(trx.rollback)
     })
+        .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
